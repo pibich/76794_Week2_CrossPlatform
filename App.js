3e16ec40a18bb964f320185835c116201709b606
PIBI <vincentbryanid@gmail.com>
2025-02-14 10:51:07 +0700

    Tugas 2
    Tugas 2

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, View, StyleSheet, Text, Image } from 'react-native';
+
+const data = [
+  { name: 'Chasca', id: '00000012345', image: require('./assets/Chasca.jpg') },
+  { name: 'Citlali', id: '00000054321', image: require('./assets/Citlali.png') },
+  { name: 'Growtopia', id: '00000023456', image: require('./assets/gt.png') },
+  { name: 'Crystal Realms', id: '00000065432', image: require('./assets/cr.png') },
+  { name: 'Engkong', id: '00000034567', image: require('./assets/Citlali.png') },
+  { name: 'Engkong', id: '00000076543', image: require('./assets/Citlali.png') }
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      {data.map((item, index) => (
+        <View key={index} style={[styles.scrollView, styles.contentContainer]}>
+          <Text>{item.name} - {item.id}</Text>
+          <Image source={item.image} style={styles.image} />
+        </View>
+      ))}
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  scrollView: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#f9f9f9',
+    marginBottom: 10,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  contentContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginTop: 10,
+  }
 });
